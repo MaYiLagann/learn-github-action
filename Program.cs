f047536d7503b6e7825229e8c9319168c6d77888
MaYiLagann <lhwshy@naver.com>
2022-07-17 15:05:19 +0900

    Create the c-sharp console project via dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_github_action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
