a489bfb9c5bf5e6e47c07a9b639620b7823fa05a
MaYiLagann <lhwshy@naver.com>
2022-07-17 17:46:38 +0900

    Add mistypo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace learn_github_action
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldo!");
         }
     }
 }
